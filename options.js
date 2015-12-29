b5d08bb8bbf4174ecf9da55ffd1e79c343eca7cc
Robin Schroer <sulami@peerwire.org>
2015-12-29 23:02:28 +0100

    prepare settings persistence

---
+++ options.js
@@ -0,0 +1,9 @@
+function save_options() {
+}
+
+function restore_options() {
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+
